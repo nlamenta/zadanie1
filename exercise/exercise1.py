2d202f29f3fe5ad3c5dd9a2232a993502979330a
Natalia Lamenta <83751@student.ue.poznan.pl>
2024-03-13 16:36:37 +0100

    pole koła

--- exercise/exercise1.py
+++ exercise/exercise1.py
@@ -6,3 +6,11 @@ bok_a = 10.5
 pole_kw = bok_a * bok_a
 
 print("Pole kwadratu wynosi: " + str(pole_kw))
+
+#pole koła
+
+r_promien = 3
+
+pole_kola =  math.pi* r_promien ** 2
+
+print("Pole koła wynosi: " + str(pole_kola))
